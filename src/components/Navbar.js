3ce65cc0a2e34225030d40270ae0dc8e4a9b419b
CleytonFerreira <jcleyton03@gmail.com>
2019-10-20 17:55:59 -0300

    correção no scroll do  navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
 import "../styles/Navbar.css";
-
+import scrollTo from 'gatsby-plugin-smoothscroll';
 
 
 export default () => (
     <Navbar collapseOnSelect expand="lg" fixed="top">
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Brand onClick={() => scrollTo('#home')}>Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <div>
                 <Nav>
-                    <Nav.Link href="#home"><AnchorLink href="#home">Home</AnchorLink></Nav.Link>
-                    <Nav.Link href="#sobre"><AnchorLink href="#sobre">Sobre</AnchorLink></Nav.Link>
-                    <Nav.Link href="#portfolio"><AnchorLink href="#portfolio">Portfolio</AnchorLink></Nav.Link>
-                    <Nav.Link href="#contato"><AnchorLink href="#contato">Contato</AnchorLink></Nav.Link>
+                    <Nav.Link onClick={() => scrollTo('#home')}>Home</Nav.Link>
+                    <Nav.Link onClick={() => scrollTo('#sobre')}>Sobre</Nav.Link>
+                    <Nav.Link onClick={() => scrollTo('#portfolio')}>Portfolio</Nav.Link>
+                    <Nav.Link onClick={() => scrollTo('#contato')}>Contato</Nav.Link>
                 </Nav>
             </div>
         </Navbar.Collapse>
