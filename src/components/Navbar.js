5ff93e3280be3af2c388acef19192e05277a43cd
CleytonFerreira <jcleyton03@gmail.com>
2019-11-03 02:21:24 -0300

    Ajustes no padding da visualização mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,15 @@ import scrollTo from 'gatsby-plugin-smoothscroll';
 
 function toggleNavbar() {
     var element = document.getElementById("responsive-navbar-nav");
+    var toggler = document.getElementById("nav-button");
     element.classList.remove("show");
+    toggler.classList.add("collapsed");
 }
 
 export default () => (
     <Navbar collapseOnSelect expand="lg" fixed="top">
         <Navbar.Brand onClick={() => { scrollTo('#home'); toggleNavbar() }}>Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle id="nav-button" aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav>
                 <Nav.Link onClick={() => { scrollTo('#home'); toggleNavbar() }}>Home</Nav.Link>
