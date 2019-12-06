fb4a2214e604a524d92739c04f477640b23a2bbb
CleytonFerreira <jcleyton03@gmail.com>
2019-12-05 21:17:40 -0300

    Refatoração do navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,11 @@ export default () => (
     <div id="home-container">
         <img src={BgHome} alt="bg-home" className="responsive" id="bg" />
         
-        <div id="slogan">
+        {/* <div id="slogan">
         <div className="vertical-line"></div>
             <h5 className="responsive">Lorem ipsum dolor sit amet</h5>
             <h5 className="responsive">Consectetuer adipiscing elit</h5>
             <h5 className="responsive">Proin dictum massa ante</h5>
-        </div>
+        </div> */}
     </div>
 )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import scrollTo from 'gatsby-plugin-smoothscroll';
 
 
-function toggleNavbar() {
-    var element = document.getElementById("responsive-navbar-nav");
-    var toggler = document.getElementById("nav-button");
-    element.classList.remove("show");
-    toggler.classList.add("collapsed");
+export default class Navbar extends Component {
+    constructor() {
+        super()
+        this.state = {
+            opened: false
+        };
+
+        this.toggle = this.toggle.bind(this)
+    }
+
+    toggle() {
+        this.setState({
+            opened: !this.state.opened
+        });
+    }
+
+    render() {
+        return (
+            <nav className="navbar">
+                <div className="nav-brand">Logo</div>
+                <ul className={`nav-links ${this.state.opened ? "opened" : "closed"}`}>
+                    <li className="nav-link">
+                        <button onClick={() => { scrollTo('#home'); this.toggle()}}>Home</button>
+                    </li>
+                    <li className="nav-link">
+                        <button onClick={() => { scrollTo('#sobre'); this.toggle()}}>Sobre</button>
+                    </li>
+                    <li className="nav-link">
+                        <button onClick={() => { scrollTo('#portfolio'); this.toggle()}}>Portfolio</button>
+                    </li>
+                    <li className="nav-link">
+                        <button onClick={() => { scrollTo('#contato'); this.toggle()}}>Contato</button>
+                    </li>
+                </ul>
+                <div className="nav-toggler"><FontAwesomeIcon icon={faBars} onClick={this.toggle} /></div>
+            </nav>
+        )
+    }
 }
-
-export default () => (
-    <Navbar collapseOnSelect expand="lg" fixed="top">
-        <Navbar.Brand onClick={() => { scrollTo('#home'); toggleNavbar() }}>Logo</Navbar.Brand>
-        <Navbar.Toggle id="nav-button" aria-controls="responsive-navbar-nav"/>
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav>
-                <Nav.Link onClick={() => { scrollTo('#home'); toggleNavbar() }}>Home</Nav.Link>
-                <Nav.Link onClick={() => { scrollTo('#sobre'); toggleNavbar() }}>Sobre</Nav.Link>
-                <Nav.Link onClick={() => { scrollTo('#portfolio'); toggleNavbar() }}>Portfolio</Nav.Link>
-                <Nav.Link onClick={() => { scrollTo('#contato'); toggleNavbar() }}>Contato</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-);
-
-
